8a0fa72a512d6c483eca073da43836c9e23836f5
hadrien gurnel <hgurnel@gmail.com>
2021-05-25 15:35:17 +0200

    Simple client/server system on two different computers

--- client.py
+++ client.py
@@ -1,35 +1,8 @@
-###!/usr/bin/env python
-##
-### IG0 side
-##
-##import socket
-##
-###hostname=socket.gethostname()   
-###TCP_IP=socket.gethostbyname(hostname) 
-##
-##TCP_IP = '192.168.1.51'
-##TCP_PORT = '12345'
-##MESSAGE = "--no-launcher"
-##
-##s = socket.create_connection((TCP_IP, TCP_PORT))
-##s.send(bytes(MESSAGE, 'utf8'))
-
-
-
-#!/usr/bin/env python
+# IG0 side
 
 import socket
 
-
-TCP_IP = '192.168.1.57'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
-
-print "received data:", data
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect(("192.168.1.51", 12345))
+message = s.recv(1024)
+print(message.decode("utf-8"))
