2f22bcac32b09fe254cc40620ae5f33e8b3abd8a
hadrien gurnel <hgurnel@gmail.com>
2021-05-25 17:14:10 +0200

    Start writing code to run executable

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 # USE ON IG1-L...IG5-R
 
 import socket
+import subprocess
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = ipv4 and SOCK_STREAM = TCP
+EXECUTABLE = "VIRUP.exe "
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 12345))
 s.listen(1) # allow for one connection to the socket
 
@@ -10,3 +14,7 @@ while True:
     clientSocket, address = s.accept()
     print(f"Connection from {address}")
     clientSocket.send(bytes("Welcome to the server", "utf-8"))
+    
+    clientData = s.recv(BUFFER_SIZE)
+    print(clientData)
+    clientSocket.send(bytes("Data received by server", "utf-8"))
